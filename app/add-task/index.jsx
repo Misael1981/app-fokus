ed16f354824c0a7c70ae9f09785f17b63df4cacb
Misael Borges <ml_bbd@hotmail.com>
2025-05-10 09:24:54 -0300

    Aula 4

--- app/add-task/index.jsx
+++ app/add-task/index.jsx
@@ -1,9 +1,71 @@
-import { Text, SafeAreaView } from "react-native-web";
+import { Pressable } from "react-native";
+import { TextInput, View, Text, SafeAreaView } from "react-native";
+import { IconSave } from "../../Components/Icons";
+import { StyleSheet } from "react-native";
+import { KeyboardAvoidingView } from "react-native";
+import { Platform } from "react-native";
+import { TouchableWithoutFeedback } from "react-native";
+import { Keyboard } from "react-native";
 
 export default function AddTask() {
   return (
-    <SafeAreaView>
-      <Text>Adicionar uma tarefa</Text>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      style={styles.container}
+      bahavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Text style={styles.text}>Adicionar uma tarefa</Text>
+          <Text style={styles.label}>Em que que você está trabalhando?</Text>
+          <TextInput style={styles.input} numberOfLines={10} multiline={true} />
+          <View style={styles.actions}>
+            <Pressable style={styles.button}>
+              <IconSave />
+              <Text>Salvar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#021123",
+    gap: 16,
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 26,
+  },
+  inner: {
+    backgroundColor: "#98a0a8",
+    width: "90%",
+    borderRadius: 8,
+    padding: 16,
+    gap: 32,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 600,
+  },
+  input: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 8,
+    height: 150,
+  },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+});
--- app/tasks/index.jsx
+++ app/tasks/index.jsx
@@ -1,9 +1,46 @@
 import { View, Text } from "react-native-web";
+import TaskItem from "../../Components/TaskItem";
+import { FokusButton } from "../../Components/FokusButton";
+import { IconPlus } from "../../Components/Icons";
+import { StyleSheet } from "react-native";
+import { router } from "expo-router";
 
 export default function Tasks() {
   return (
-    <View>
-      <Text>Página para listas tarefas</Text>
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.text}>Lista de Tarefas</Text>
+        <View style={styles.inner}>
+          <TaskItem completed text={"Estudar React"} />
+          <TaskItem text={"Estudar React Native"} />
+        </View>
+        <FokusButton
+          title={"Adicionar nova tarefa"}
+          icon={<IconPlus outline />}
+          outline
+          onPress={() => router.navigate("/add-task")}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#021123",
+  },
+  wrapper: {
+    gap: 40,
+    width: "90%",
+  },
+  text: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 26,
+  },
+  inner: {
+    gap: 8,
+  },
+});
